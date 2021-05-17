ffe29e5696691c8a5b9e1138f79bfd3659fb8de7
victor-jvp <ing.vj15@gmail.com>
2021-05-17 02:08:07 -0400

    Added migration to recibos tables

--- app/Models/ReciboCab.php
+++ app/Models/ReciboCab.php
@@ -4,15 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class ReciboCab extends Model
 {
-    public $timestamps = false;
-
     protected $table = 'recibos_cab';
 
     public function reciboDet()
     {
         return $this->hasMany(ReciboDet::class, "ID_RECIBO");
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+        static::creating(function($model)
+        {
+            $user = Auth::user();
+            $model->created_by = $user->id;
+            $model->updated_by = $user->id;
+        });
+        static::updating(function($model)
+        {
+            $user = Auth::user();
+            $model->updated_by = $user->id;
+        });
     }
 }
