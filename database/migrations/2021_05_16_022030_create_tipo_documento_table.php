8c75693c2a13affe26e181a3dbc0872265c76803
Victor Veliz <ing.vj15@gmail.com>
2021-06-01 22:29:24 -0400

    change migrations

---
+++ database/migrations/2021_05_16_022030_create_tipo_documento_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTipoDocumentoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tipo_documento', function (Blueprint $table) {
+            $table->string('TIPO_DOC', 6)->primary();
+            $table->string('DESCR');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tipo_documento');
+    }
+}
